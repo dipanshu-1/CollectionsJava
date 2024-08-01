0d6b21be65b395d7538d60150656899e9a6968ff
Dipanshu 1 <Dipanshu.1@godigit.com>
2024-08-01 14:36:47 +0530

    Binaray number generation using queue

---
+++ Queue/com/GenerateBinaryUsingQueue.java
@@ -0,0 +1,30 @@
+package Queue.com;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+public class GenerateBinaryUsingQueue {
+
+        public static void generate(int n)
+        {
+
+            Queue<String> q = new ArrayDeque<>();
+            q.add("1");
+
+
+            int i = 1;
+            while (i++ <= n)
+            {
+                
+                q.add(q.peek() + '0');
+                q.add(q.peek() + '1');
+
+                // remove the front element and print it
+                System.out.print(q.poll() + ' ');
+            }
+        }
+
+    public static void main(String[] args) {
+        int n=16;
+        generate(n);
+    }
+}
